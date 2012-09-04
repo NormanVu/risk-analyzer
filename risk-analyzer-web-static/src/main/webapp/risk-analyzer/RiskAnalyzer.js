0da73bfea0f5573300d3761fee58ccabed06c0e3
Daniel Pacak <pacak.daniel@gmail.com>
2012-09-04 20:52:47 +0200

    Formatting RiskAnalyzer.js

--- risk-analyzer-web-static/src/main/webapp/risk-analyzer/RiskAnalyzer.js
+++ risk-analyzer-web-static/src/main/webapp/risk-analyzer/RiskAnalyzer.js
@@ -1,31 +1,34 @@
 /**
  * The main RiskAnalyzer application.
+ * 
  * @author Daniel Pacak
  */
 Ext.define('RiskAnalyzer.App', {
-  extend: 'Ext.container.Viewport',
+	extend : 'Ext.container.Viewport',
 
-  initComponent: function(){
-    Ext.apply(this, {
-      layout: 'border',
-      padding: 5,
-      items: [
-{
-    xtype: 'box',
-    id: 'header',
-    region: 'north',
-    html: '<h1>Risk Analyzer - 0.0.1-SNAPSHOT</h1>',
-    height: 30},
+	initComponent : function() {
+		Ext.apply(this, {
+			layout : 'border',
+			padding : 5,
+			items : [ {
+				xtype : 'box',
+				id : 'header',
+				region : 'north',
+				html : '<h1>Risk Analyzer - 0.0.1-SNAPSHOT</h1>',
+				height : 30
+			},
 
-              this.createMainPanel()]
-    });
+			this.createMainPanel() ]
+		});
 
-    this.callParent(arguments);
-  },
+		this.callParent(arguments);
+	},
 
-  createMainPanel: function() {
-    this.mainPanel = Ext.create('widget.mainpanel', {region: 'center'});
-    return this.mainPanel;
-  }
+	createMainPanel : function() {
+		this.mainPanel = Ext.create('widget.mainpanel', {
+			region : 'center'
+		});
+		return this.mainPanel;
+	}
 
 });
