ac90cd1af417e0aea20ae985ab73fad9bc93042a
Daniel Pacak <pacak.daniel@gmail.com>
2012-09-04 20:51:10 +0200

    Updating copyright notice

--- risk-analyzer-web-static/src/main/webapp/risk-analyzer/HelpWindow.js
+++ risk-analyzer-web-static/src/main/webapp/risk-analyzer/HelpWindow.js
@@ -13,7 +13,7 @@ Ext.define('RiskAnalyzer.HelpWindow', {
         iconCls: 'feed',
         layout: 'fit',
         html: '<br/><center><strong>Risk Analyzer - 0.0.1-SNAPSHOT</strong></center>' +
-              "<br/><center>Copyright 2010-2011 <a href='mailto:info@scirisk.com'>SciRisk</a>, Inc. All rights reserved.</center>" +
+              "<br/><center>Copyright 2010-2012 <a href='mailto:info@scirisk.com'>SciRisk</a>, Inc. All rights reserved.</center>" +
               "<br/><br/><br/><br/><br/><br/><br/><center style='font-size: 9px'>UI Design &amp; Development</center>" +
               "<center style='font-size: 9px'><a href='mailto:pacak.daniel@gmail.com'>Daniel Pacak</a></center>",
         buttons: [{
