9877acc15506ec050e1de953448f9c18e1f07fe7
Daniel Pacak <pacak.daniel@gmail.com>
2013-01-04 20:09:45 +0100

    fixing google repo impl

--- risk-analyzer-frontend-repository/risk-analyzer-frontend-repository-google/src/main/java/com/danielpacak/riskanalyzer/frontend/repository/google/FacilityRepositoryGoogleImpl.java
+++ risk-analyzer-frontend-repository/risk-analyzer-frontend-repository-google/src/main/java/com/danielpacak/riskanalyzer/frontend/repository/google/FacilityRepositoryGoogleImpl.java
@@ -4,25 +4,18 @@ import java.util.List;
 
 import com.danielpacak.riskanalyzer.domain.Facility;
 import com.danielpacak.riskanalyzer.frontend.repository.FacilityRepository;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.scirisk.google.persistence.EntityManager;
-import com.scirisk.google.persistence.EntityManagerImpl;
 
 public class FacilityRepositoryGoogleImpl implements FacilityRepository {
 
-	private DatastoreService datastoreService;
-	private EntityManager em;
 	private GoogleDatastoreTemplate datastoreTemplate;
 
-	public FacilityRepositoryGoogleImpl(DatastoreService datastoreService) {
-		this.datastoreService = datastoreService;
-		this.em = new EntityManagerImpl(datastoreService);
-		this.datastoreTemplate = new GoogleDatastoreTemplate(datastoreService);
+	public FacilityRepositoryGoogleImpl(GoogleDatastoreTemplate datastoreTemplate) {
+		this.datastoreTemplate = datastoreTemplate;
 	}
 
 	@Override
 	public Facility save(Facility facility) {
-		return em.save(facility);
+		return datastoreTemplate.put(facility, new FacilityWriteConverter());
 	}
 
 	@Override
---
+++ risk-analyzer-frontend-repository/risk-analyzer-frontend-repository-google/src/main/java/com/danielpacak/riskanalyzer/frontend/repository/google/FacilityWriteConverter.java
@@ -0,0 +1,30 @@
+package com.danielpacak.riskanalyzer.frontend.repository.google;
+
+import com.danielpacak.riskanalyzer.domain.Facility;
+import com.danielpacak.riskanalyzer.frontend.repository.google.GoogleDatastoreTemplate.Converter;
+import com.google.appengine.api.datastore.Entity;
+
+/** Converts facility entity to the google datastore entity. */
+public class FacilityWriteConverter implements Converter<Facility, Entity> {
+
+	@Override
+	public Entity convert(Facility facility) {
+		Entity entity = new Entity(Facility.class.getName());
+		entity.setProperty("type", facility.getType().name());
+		entity.setProperty("kind", facility.getKind().name());
+		entity.setProperty("name", facility.getName());
+		entity.setProperty("description", facility.getDescription());
+		entity.setProperty("address", facility.getAddress());
+		entity.setProperty("latitude", facility.getLatitude());
+		entity.setProperty("longitude", facility.getLongitude());
+		entity.setProperty("riskCategory1", facility.getRiskCategory1());
+		entity.setProperty("riskCategory2", facility.getRiskCategory2());
+		entity.setProperty("riskCategory3", facility.getRiskCategory3());
+		entity.setProperty("recoveryTime1", facility.getRecoveryTime1());
+		entity.setProperty("recoveryTime2", facility.getRecoveryTime2());
+		entity.setProperty("recoveryTime3", facility.getRecoveryTime3());
+
+		return entity;
+	}
+
+}
