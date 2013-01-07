452e73aa2c1b7adfbdf58fc5c60a7bb62b40b1d6
dpacak <pacak.daniel@gmail.com>
2013-01-07 11:07:28 +0100

    adding tests

--- risk-analyzer-frontend-repository/risk-analyzer-frontend-repository-google/src/test/java/com/danielpacak/riskanalyzer/frontend/repository/google/FacilityRepositoryGoogleImplTest.java
+++ risk-analyzer-frontend-repository/risk-analyzer-frontend-repository-google/src/test/java/com/danielpacak/riskanalyzer/frontend/repository/google/FacilityRepositoryGoogleImplTest.java
@@ -1,60 +1,54 @@
 package com.danielpacak.riskanalyzer.frontend.repository.google;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.danielpacak.riskanalyzer.domain.DistributionChannel;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.danielpacak.riskanalyzer.domain.Facility;
 
 // TODO Implement tests
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ KeyFactory.class, Key.class })
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest({ KeyFactory.class, Key.class })
 public class FacilityRepositoryGoogleImplTest {
 
-	/*@Test
+	GoogleDatastoreTemplate mockDatastoreTemplate;
+	FacilityRepositoryGoogleImpl repository;
+
+	@Before
+	public void beforeEachTest() throws Exception {
+		mockDatastoreTemplate = mock(GoogleDatastoreTemplate.class);
+		repository = new FacilityRepositoryGoogleImpl(mockDatastoreTemplate);
+	}
+
+	@Test
+	public void testSave() throws Exception {
+		Facility facility = new Facility();
+		repository.save(facility);
+		verify(mockDatastoreTemplate).put(eq(facility), isA(FacilityWriteConverter.class));
+	}
+
+	@Test
 	public void testDelete() throws Exception {
-		String facilityId = "13";
-		DatastoreService datastoreService = mock(DatastoreService.class);
-		Transaction currentTransaction = mock(Transaction.class);
-		when(datastoreService.getCurrentTransaction()).thenReturn(
-				currentTransaction);
+		String facilityId = "666";
+		repository.delete(facilityId);
+		verify(mockDatastoreTemplate).delete(Facility.class, facilityId);
+	}
 
-		mockStatic(KeyFactory.class);
-		Key key = mock(Key.class);
-		when(KeyFactory.createKey(FACILITY_ENTITY, Long.valueOf(facilityId))).thenReturn(key);
+	@Test
+	public void testFindOne() throws Exception {
+		String facilityId = "666";
+		repository.findOne(facilityId);
+		verify(mockDatastoreTemplate).findById(eq(facilityId), eq(Facility.class), isA(FacilityReadConverter.class));
+	}
 
-		FacilityRepositoryGoogleImpl facilityRepository = new FacilityRepositoryGoogleImpl(
-				datastoreService);
-
-		facilityRepository.delete(facilityId);
-		InOrder inOrder = inOrder(datastoreService, currentTransaction);
-		inOrder.verify(datastoreService).beginTransaction();
-		inOrder.verify(datastoreService).delete(key);
-		inOrder.verify(currentTransaction).commit();
-	}*/
-	
 	@Test
 	public void testFindAll() throws Exception {
-		DatastoreService datastoreService = mock(DatastoreService.class);
-		System.out.println(DistributionChannel.class.getSimpleName());
+		repository.findAll();
+		verify(mockDatastoreTemplate).findAll(eq(Facility.class), isA(FacilityReadConverter.class));
 	}
-	
-	
-	/*public List<Facility> findAll() {
-		Query q = new Query(FACILITY_ENTITY);
-		PreparedQuery pq = datastoreService.prepare(q);
-		List<Facility> nodes = new ArrayList<Facility>();
-		for (Entity nodeEntity : pq.asIterable()) {
-			Facility node = map(nodeEntity);
-			nodes.add(node);
-		}
-		return nodes;
-	}*/
 
 }
