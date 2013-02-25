ac1231e889dde9197e0e8cfef6833e5e5741e712
dpacak <pacak.daniel@gmail.com>
2013-02-25 16:26:33 +0100

    Polishing spring security config

--- calendar/src/main/java/com/example/calendar/web/IndexController.java
+++ calendar/src/main/java/com/example/calendar/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.calendar.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 
 	@RequestMapping(value = "/index.do", method = { RequestMethod.GET })
-	public String indexPage() {
+	public String indexPage(HttpServletRequest request) {
+		System.out.println("user principal: " + request.getUserPrincipal());
+		System.out.println("is user admin: " + request.isUserInRole("ROLE_ADMIN"));
 		return "index";
 	}
 
